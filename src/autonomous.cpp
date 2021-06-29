0084ff4ca1cd41b6a511dcf46c577ea4059e8ba7
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2021-06-29 17:16:00 -0400

    added sample autonomous

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -1,5 +1,12 @@
 #include "main.h"
+#include "globals.h"
 
 void myAuton() {
-    // TODO: Add basic auton with PID + Odom
+    driveTrainPID.moveToPoint(Vector2(1, 1)); // Go to 1ft x 1ft
+    
+    pros::delay(40);
+
+    driveTrainPID.rotateTo(degToRad(90)); // Rotate to 90 degrees
+
+    driveTrainPID.moveToOrientation(Vector2(10, 10), degToRad(95)); // Go to 10ft x 10ft and settle at a 95 degree rotation
 }
