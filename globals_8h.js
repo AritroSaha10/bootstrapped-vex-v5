c8b8380f0673b6868df0636657c53cc0543e1a66
AritroSaha10 <AritroSaha10@users.noreply.github.com>
2021-07-08 16:15:13 +0000

    deploy: 3fbf5b950d47de1d93d72747fff455486fa30357

--- globals_8h.js
+++ globals_8h.js
@@ -7,6 +7,7 @@ var globals_8h =
     [ "driveTrain", "globals_8h.html#a9918f135d7fdcb02728d42fd28a8f9f5", null ],
     [ "driveTrainPID", "globals_8h.html#a4cbeb7cf18980bd46a2804e0b1210674", null ],
     [ "lEnc", "globals_8h.html#a34584f147dfae2bd31583a5d75822fb8", null ],
+    [ "masterController", "globals_8h.html#aa596e207060fbc64178eae4e23ffd708", null ],
     [ "rEnc", "globals_8h.html#aac9fab541047b8f67b29107b5699be03", null ],
     [ "tLeft", "globals_8h.html#a7f6f9a082eebdc5bdac372a0c21ffe4c", null ],
     [ "trackingData", "globals_8h.html#a1360d607e875fb4f719674afce4b8317", null ],
